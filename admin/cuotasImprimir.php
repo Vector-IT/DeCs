4384f77bab1092a3c180b290fac81f6af9c8c8b2
José Romero <jose.romero@vector-it.com.ar>
2017-02-24 19:08:34 -0300

    Cambios plantilla

--- admin/cuotasImprimir.php
+++ admin/cuotasImprimir.php
@@ -78,57 +78,57 @@
 
 		$pdf->SetFont('Times', 'B', 8);
 		//NumeClie
-		$pdf->Text(20, 46, $cuota["NumeClie"]);
+		$pdf->Text(20, 43, $cuota["NumeClie"]);
 		//NumeCuot
-		$pdf->Text(60, 46, $cuota["NumeCuot"]);
+		$pdf->Text(60, 43, $cuota["NumeCuot"]);
 		//FechVenc1
-		$pdf->Text(101, 47, $cuota["FechVenc1"]);
+		$pdf->Text(101, 44, $cuota["FechVenc1"]);
 		//FechVenc2
-		$pdf->Text(119, 47, $cuota["FechVenc2"]);
+		$pdf->Text(121, 44, $cuota["FechVenc2"]);
 		//FechVenc3
-		$pdf->Text(136, 47, $cuota["FechVenc3"]);
+		$pdf->Text(139, 44, $cuota["FechVenc3"]);
 		//ImpoTota1
-		$pdf->Text(101, 52, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(101, 49, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//ImpoTota2
-		$pdf->Text(119, 52, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(121, 49, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//ImpoTota3
-		$pdf->Text(136, 52, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(139, 49, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//NumePago
-		$pdf->Text(93, 65, $cuota["NumePago"]);
+		$pdf->Text(93, 62, $cuota["NumePago"]);
 		//FechPago
-		$pdf->Text(120, 65, $cuota["FechPago"]);
+		$pdf->Text(121, 62, $cuota["FechPago"]);
 		//ValoMovi
-		$pdf->Text(141, 65, $cliente["ValoMovi"]);
+		$pdf->Text(143, 62, $cliente["ValoMovi"]);
 		//NombClie
-		$pdf->Text(23, 56, utf8_decode($cliente["NombClie"]));
+		$pdf->Text(23, 54, utf8_decode($cliente["NombClie"]));
 		//DireClie
-		$pdf->Text(23, 59, utf8_decode($cliente["DireClie"]));
+		$pdf->Text(23, 57, utf8_decode($cliente["DireClie"]));
 		//NombBarr
-		$pdf->Text(23, 62, utf8_decode($cliente["NombBarr"]));
+		$pdf->Text(23, 60, utf8_decode($cliente["NombBarr"]));
 		//NombLoca - CodiPost
-		$pdf->Text(23, 65, utf8_decode($cliente["NombLoca"]. " - " .$cliente["CodiPost"]));
+		$pdf->Text(23, 63, utf8_decode($cliente["NombLoca"]. " - " .$cliente["CodiPost"]));
 		//NombProv
-		$pdf->Text(23, 68, utf8_decode($cliente["NombProv"]));
+		$pdf->Text(23, 66, utf8_decode($cliente["NombProv"]));
 		//ImpoPura
-		$pdf->SetXY(185, 52);
+		$pdf->SetXY(185, 50);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoPura"]), 2), 0, 0, 'R');
 		//ImpoAdmi
-		$pdf->SetXY(185, 56);
+		$pdf->SetXY(185, 54);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoAdmi"]), 2), 0, 0, 'R');
 		//ImpoGest
-		$pdf->SetXY(185, 59);
+		$pdf->SetXY(185, 57);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoGest"]), 2), 0, 0, 'R');
 		//ImpoOtro
-		$pdf->SetXY(185, 62);
+		$pdf->SetXY(185, 60);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoOtro"]), 2), 0, 0, 'R');
 		//ImpoTota
-		$pdf->Text(157, 68, 'Total:');
-		$pdf->SetXY(185, 68);
+		$pdf->Text(159, 66, 'Total:');
+		$pdf->SetXY(185, 66);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoTota"]), 2), 0, 0, 'R');
 
 		//CodiBarr
-		$pdf->GDImage(barcode("", $cuota["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 75, 135, 5);
-		$pdf->Text(50, 83, $cuota["CodiBarr"]);
+		$pdf->GDImage(barcode("", $cuota["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 72, 135, 7);
+		$pdf->Text(50, 82, $cuota["CodiBarr"]);
 
 	//Talon 2
 	// Y = Talon1 + 48
@@ -138,56 +138,56 @@
 		}
 
 		//NumeClie
-		$pdf->Text(20, 94, $cuota["NumeClie"]);
+		$pdf->Text(20, 92, $cuota["NumeClie"]);
 		//NumeCuot
-		$pdf->Text(60, 94, $cuota["NumeCuot"]);
+		$pdf->Text(60, 92, $cuota["NumeCuot"]);
 		//FechVenc1
-		$pdf->Text(101, 94, $cuota["FechVenc1"]);
+		$pdf->Text(101, 92, $cuota["FechVenc1"]);
 		//FechVenc2
-		$pdf->Text(119, 94, $cuota["FechVenc2"]);
+		$pdf->Text(121, 92, $cuota["FechVenc2"]);
 		//FechVenc3
-		$pdf->Text(136, 94, $cuota["FechVenc3"]);
+		$pdf->Text(139, 92, $cuota["FechVenc3"]);
 		//ImpoTota1
-		$pdf->Text(101, 100, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(101, 97, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//ImpoTota2
-		$pdf->Text(119, 100, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(121, 97, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//ImpoTota3
-		$pdf->Text(136, 100, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(139, 97, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//NumePago
-		$pdf->Text(93, 113, $cuota["NumePago"]);
+		$pdf->Text(93, 111, $cuota["NumePago"]);
 		//FechPago
-		$pdf->Text(120, 113, $cuota["FechPago"]);
+		$pdf->Text(121, 111, $cuota["FechPago"]);
 		//ValoMovi
-		$pdf->Text(141, 113, $cliente["ValoMovi"]);
+		$pdf->Text(143, 111, $cliente["ValoMovi"]);
 		//NombClie
-		$pdf->Text(23, 102, utf8_decode($cliente["NombClie"]));
+		$pdf->Text(23, 100, utf8_decode($cliente["NombClie"]));
 		//DireClie
-		$pdf->Text(23, 105, utf8_decode($cliente["DireClie"]));
+		$pdf->Text(23, 103, utf8_decode($cliente["DireClie"]));
 		//NombBarr
-		$pdf->Text(23, 108, utf8_decode($cliente["NombBarr"]));
+		$pdf->Text(23, 106, utf8_decode($cliente["NombBarr"]));
 		//NombLoca - CodiPost
-		$pdf->Text(23, 111, utf8_decode($cliente["NombLoca"]. " - " .$cliente["CodiPost"]));
+		$pdf->Text(23, 109, utf8_decode($cliente["NombLoca"]. " - " .$cliente["CodiPost"]));
 		//NombProv
-		$pdf->Text(23, 114, utf8_decode($cliente["NombProv"]));
+		$pdf->Text(23, 112, utf8_decode($cliente["NombProv"]));
 		//ImpoPura
-		$pdf->SetXY(185, 98);
+		$pdf->SetXY(185, 96);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoPura"]), 2), 0, 0, 'R');
 		//ImpoAdmi
-		$pdf->SetXY(185, 102);
+		$pdf->SetXY(185, 100);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoAdmi"]), 2), 0, 0, 'R');
 		//ImpoGest
-		$pdf->SetXY(185, 105);
+		$pdf->SetXY(185, 103);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoGest"]), 2), 0, 0, 'R');
 		//ImpoOtro
-		$pdf->SetXY(185, 108);
+		$pdf->SetXY(185, 106);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoOtro"]), 2), 0, 0, 'R');
 		//ImpoTota
-		$pdf->Text(157, 114, 'Total:');
-		$pdf->SetXY(185, 114);
+		$pdf->Text(159, 112, 'Total:');
+		$pdf->SetXY(185, 112);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoTota"]), 2), 0, 0, 'R');
 
 		//CodiBarr
-		$pdf->GDImage(barcode("", $cuota["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 121, 135, 5);
+		$pdf->GDImage(barcode("", $cuota["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 119, 135, 7);
 		$pdf->Text(50, 129, $cuota["CodiBarr"]);
 
 	//Talon 3
@@ -198,27 +198,27 @@
 		}
 
 		//NumeClie
-		$pdf->Text(20, 139, $cuota["NumeClie"]);
+		$pdf->Text(20, 138, $cuota["NumeClie"]);
 		//NumeCuot
-		$pdf->Text(60, 139, $cuota["NumeCuot"]);
+		$pdf->Text(60, 138, $cuota["NumeCuot"]);
 		//FechVenc1
-		$pdf->Text(101, 139, $cuota["FechVenc1"]);
+		$pdf->Text(101, 138, $cuota["FechVenc1"]);
 		//FechVenc2
-		$pdf->Text(119, 139, $cuota["FechVenc2"]);
+		$pdf->Text(121, 138, $cuota["FechVenc2"]);
 		//FechVenc3
-		$pdf->Text(136, 139, $cuota["FechVenc3"]);
+		$pdf->Text(139, 138, $cuota["FechVenc3"]);
 		//ImpoTota1
-		$pdf->Text(101, 145, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(101, 143, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//ImpoTota2
-		$pdf->Text(119, 145, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(121, 143, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//ImpoTota3
-		$pdf->Text(136, 145, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(139, 143, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//NumePago
 		$pdf->Text(93, 158, $cuota["NumePago"]);
 		//FechPago
 		$pdf->Text(120, 158, $cuota["FechPago"]);
 		//ValoMovi
-		$pdf->Text(141, 158, $cliente["ValoMovi"]);
+		$pdf->Text(143, 158, $cliente["ValoMovi"]);
 		//NombClie
 		$pdf->Text(23, 147, utf8_decode($cliente["NombClie"]));
 		//DireClie
@@ -230,25 +230,25 @@
 		//NombProv
 		$pdf->Text(23, 159, utf8_decode($cliente["NombProv"]));
 		//ImpoPura
-		$pdf->SetXY(185, 143);
+		$pdf->SetXY(185, 142);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoPura"]), 2), 0, 0, 'R');
 		//ImpoAdmi
-		$pdf->SetXY(185, 147);
+		$pdf->SetXY(185, 146);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoAdmi"]), 2), 0, 0, 'R');
 		//ImpoGest
-		$pdf->SetXY(185, 150);
+		$pdf->SetXY(185, 149);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoGest"]), 2), 0, 0, 'R');
 		//ImpoOtro
-		$pdf->SetXY(185, 153);
+		$pdf->SetXY(185, 152);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoOtro"]), 2), 0, 0, 'R');
 		//ImpoTota
-		$pdf->Text(157, 159, 'Total:');
-		$pdf->SetXY(185, 159);
+		$pdf->Text(159, 158, 'Total:');
+		$pdf->SetXY(185, 158);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoTota"]), 2), 0, 0, 'R');
 
 		//CodiBarr
-		$pdf->GDImage(barcode("", $cuota["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 166, 135, 5);
-		$pdf->Text(50, 174, $cuota["CodiBarr"]);
+		$pdf->GDImage(barcode("", $cuota["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 165, 135, 7);
+		$pdf->Text(50, 175, $cuota["CodiBarr"]);
 
 	//Talon 4
 	//Y = Talon3 + 45
@@ -258,62 +258,62 @@
 		}
 
 		//NumeClie
-		$pdf->Text(20, 184, $cuota["NumeClie"]);
+		$pdf->Text(20, 185, $cuota["NumeClie"]);
 		//NumeCuot
-		$pdf->Text(60, 184, $cuota["NumeCuot"]);
+		$pdf->Text(60, 185, $cuota["NumeCuot"]);
 		//FechVenc1
-		$pdf->Text(101, 184, $cuota["FechVenc1"]);
+		$pdf->Text(101, 185, $cuota["FechVenc1"]);
 		//FechVenc2
-		$pdf->Text(119, 184, $cuota["FechVenc2"]);
+		$pdf->Text(121, 185, $cuota["FechVenc2"]);
 		//FechVenc3
-		$pdf->Text(136, 184, $cuota["FechVenc3"]);
+		$pdf->Text(139, 185, $cuota["FechVenc3"]);
 		//ImpoTota1
 		$pdf->Text(101, 190, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//ImpoTota2
-		$pdf->Text(119, 190, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(121, 190, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//ImpoTota3
-		$pdf->Text(136, 190, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
+		$pdf->Text(139, 190, "$ ".number_format(floatval($cuota["ImpoTota"]), 2));
 		//NumePago
-		$pdf->Text(93, 203, $cuota["NumePago"]);
+		$pdf->Text(93, 204, $cuota["NumePago"]);
 		//FechPago
-		$pdf->Text(120, 203, $cuota["FechPago"]);
+		$pdf->Text(120, 204, $cuota["FechPago"]);
 		//ValoMovi
-		$pdf->Text(141, 203, $cliente["ValoMovi"]);
+		$pdf->Text(143, 204, $cliente["ValoMovi"]);
 		//NombClie
-		$pdf->Text(23, 192, utf8_decode($cliente["NombClie"]));
+		$pdf->Text(23, 194, utf8_decode($cliente["NombClie"]));
 		//DireClie
-		$pdf->Text(23, 195, utf8_decode($cliente["DireClie"]));
+		$pdf->Text(23, 197, utf8_decode($cliente["DireClie"]));
 		//NombBarr
-		$pdf->Text(23, 198, utf8_decode($cliente["NombBarr"]));
+		$pdf->Text(23, 200, utf8_decode($cliente["NombBarr"]));
 		//NombLoca - CodiPost
-		$pdf->Text(23, 201, utf8_decode($cliente["NombLoca"]. " - " .$cliente["CodiPost"]));
+		$pdf->Text(23, 203, utf8_decode($cliente["NombLoca"]. " - " .$cliente["CodiPost"]));
 		//NombProv
-		$pdf->Text(23, 204, utf8_decode($cliente["NombProv"]));
+		$pdf->Text(23, 206, utf8_decode($cliente["NombProv"]));
 		//ImpoPura
-		$pdf->SetXY(185, 188);
+		$pdf->SetXY(185, 189);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoPura"]), 2), 0, 0, 'R');
 		//ImpoAdmi
-		$pdf->SetXY(185, 192);
+		$pdf->SetXY(185, 193);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoAdmi"]), 2), 0, 0, 'R');
 		//ImpoGest
-		$pdf->SetXY(185, 195);
+		$pdf->SetXY(185, 196);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoGest"]), 2), 0, 0, 'R');
 		//ImpoOtro
-		$pdf->SetXY(185, 198);
+		$pdf->SetXY(185, 199);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoOtro"]), 2), 0, 0, 'R');
 		//ImpoTota
-		$pdf->Text(157, 204, 'Total:');
-		$pdf->SetXY(185, 204);
+		$pdf->Text(159, 205, 'Total:');
+		$pdf->SetXY(185, 205);
 		$pdf->Cell(17, 0, "$ ".number_format(floatval($cuota["ImpoTota"]), 2), 0, 0, 'R');
 
 		//CodiBarr
-		$pdf->GDImage(barcode("", $cuota["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 211, 135, 5);
-		$pdf->Text(50, 219, $cuota["CodiBarr"]);
+		$pdf->GDImage(barcode("", $cuota["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 211, 135, 7);
+		$pdf->Text(50, 221, $cuota["CodiBarr"]);
 
 	//Final
 		fin:
 		//CodiBarrClie
-		$pdf->GDImage(barcode("", $cliente["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 230, 135, 5);
+		$pdf->GDImage(barcode("", $cliente["CodiBarr"], 10, "horizontal", "code128", false, 1, "gd"), 25, 231, 135, 7);
 		$pdf->Text(50, 238, $cliente["CodiBarr"]);
 
 		//CodiPagoElec
